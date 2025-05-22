3aedf5e6e621cba6164da95b13e13b5f98922e13
Andrew Nessin <andrewnessinjim@yahoo.com>
2025-05-22 20:53:40 +0530

    Student mutation services

--- server/src/resolvers/studentResolvers.ts
+++ server/src/resolvers/studentResolvers.ts
@@ -1,4 +1,10 @@
-import { fetchStudents } from "../services/studentServices";
+import {
+  deleteStudent,
+  fetchStudent,
+  fetchStudents,
+  saveStudent,
+  updateStudent,
+} from "../services/studentServices";
 import { Resolvers } from "../generated/graphql_types";
 
 const books = [
@@ -15,8 +21,23 @@ const books = [
 const studentResolvers: Resolvers = {
   Query: {
     books: () => books,
-    students: async (_, __dirname, { pgPool }) => {
+    students: async (_, __, { pgPool }) => {
       return fetchStudents(pgPool);
+    },
+    student: async (_, { id: studentId }, { pgPool }) => {
+      return fetchStudent(pgPool, studentId);
+    },
+  },
+  Mutation: {
+    addStudent: async (_, { name, age, class_id }, { pgPool }) => {
+      return saveStudent(pgPool, name, age, class_id);
+    },
+    updateStudent: async (_, { id, name, age, class_id }, { pgPool }) => {
+      console.log("updateStudent", id, name, age, class_id);
+      return updateStudent(pgPool, id, name, age, class_id);
+    },
+    deleteStudent: async (_, { id }, { pgPool }) => {
+      return deleteStudent(pgPool, id);
     },
   },
 };
--- server/src/services/studentServices.ts
+++ server/src/services/studentServices.ts
@@ -6,3 +6,73 @@ export async function fetchStudents(pgPool: Pool) {
   );
   return result.rows;
 }
+
+export async function fetchStudent(pgPool: Pool, studentId: string) {
+  const result = await pgPool.query(
+    "SELECT id, name, age, class_id FROM students WHERE id = $1",
+    [studentId]
+  );
+  return result.rows[0];
+}
+
+export async function saveStudent(
+  pgPool: Pool,
+  name: string,
+  age: number,
+  class_id?: string | null
+) {
+  const result = await pgPool.query(
+    "INSERT INTO students (name, age, class_id) VALUES ($1, $2, $3) RETURNING *",
+    [name, age, class_id]
+  );
+  return result.rows[0];
+}
+
+export async function deleteStudent(pgPool: Pool, studentId: string) {
+  const result = await pgPool.query(
+    "DELETE FROM students WHERE id = $1 RETURNING *",
+    [studentId]
+  );
+
+  return result.rowCount !== 0;
+}
+
+export async function updateStudent(
+  pgPool: Pool,
+  studentId: string,
+  name?: string | null,
+  age?: number | null,
+  class_id?: string | null
+) {
+  const fieldsToUpdate = [];
+  const values = [];
+  let paramIndex = 1;
+  if (name !== undefined) {
+    fieldsToUpdate.push(`name = $${paramIndex}`);
+    values.push(name);
+    paramIndex++;
+  }
+
+  if (age !== undefined) {
+    fieldsToUpdate.push(`age = $${paramIndex}`);
+    values.push(age);
+    paramIndex++;
+  }
+
+  if (class_id !== undefined) {
+    fieldsToUpdate.push(`class_id = $${paramIndex}`);
+    values.push(class_id);
+    paramIndex++;
+  }
+
+  if (fieldsToUpdate.length === 0) {
+    throw new Error("No fields to update");
+  }
+  const setClause = fieldsToUpdate.join(", ");
+  values.push(studentId);
+  const query = `UPDATE students SET ${setClause} WHERE id = $${paramIndex} RETURNING *`;
+  console.log("query", query);
+  console.log("values", values);
+  const result = await pgPool.query(query, values);
+  return result.rows[0];
+}
